48b4fc7774d91bdb5a176e33137ad2e62a6ea64e
Praveen Kumar <praveen@Praveens-MacBook-Pro.local>
2021-12-19 10:18:44 +0530

    Added Google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -4,12 +4,35 @@ import '../styles/globals.css'
 // import '@styles/prism-one-dark.css'
 import '@styles/prism-one-light.css'
 import '@styles/extra-syntax.css'
-
+import { useEffect } from 'react'
+import Script from 'next/script'
+import { useRouter } from 'next/router'
+import * as gtag from '@utils/gtag.js'
 
 function MyApp({ Component, pageProps }) {
-  return <HomeLayout>
-    <Component {...pageProps} />
-  </HomeLayout>
+  return <>
+    <Script
+      strategy="afterInteractive"
+      src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+    />
+    <Script
+      id="gtag-init"
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{
+        __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+      }}
+    />
+    <HomeLayout>
+      <Component {...pageProps} />
+    </HomeLayout>
+  </>
 }
 
 export default MyApp
