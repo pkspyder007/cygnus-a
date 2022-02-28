057f3668216fe76321cfe71f8b9cf7793409af57
Praveen Kumar <praveen@quizizz.com>
2022-02-28 12:20:05 +0530

    gtag

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -5,6 +5,7 @@ class MyDocument extends Document {
         return (
             <Html className="scroll-smooth selection:bg-emerald-100 selection:text-green dark:selection:bg-blue-300 dark:selection:text-blue-600">
                 <Head>
+                <meta name="google-site-verification" content="6rSQiloY-B0JoQb2gJpzVTEuFzW94WUZfM8-csRRsic" />
                     <link
                         href="https://fonts.googleapis.com/css2?family=Inter&display=optional"
                         rel="stylesheet"
